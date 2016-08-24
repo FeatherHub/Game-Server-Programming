098594328e12421ff47431f95785982256037ab7
FeatherHub <woook95@naver.com>
2016-08-24 23:58:43 +0900

    Add new packetid and packet size

--- TermProject/Common/Util/BodySizeManager.cpp
+++ TermProject/Common/Util/BodySizeManager.cpp
@@ -11,8 +11,13 @@ void BodySizeManager::Init()
 {
 	m_bodySizeArr[PacketId::TestReq] = sizeof(TestReqPkt);
 	m_bodySizeArr[PacketId::TestRes] = sizeof(TestResPkt);
+
 	m_bodySizeArr[PacketId::LoginReq] = sizeof(LoginReqPkt);
 	m_bodySizeArr[PacketId::LoginRes] = sizeof(LoginResPkt);
+	m_bodySizeArr[PacketId::LoginNtf] = sizeof(LoginNtfPkt);
+	
+	m_bodySizeArr[PacketId::LobbyUserNameListReq] = sizeof(LobbyUserIdListReqPkt);
+	m_bodySizeArr[PacketId::LobbyUserNameListRes] = sizeof(LobbyUserIdListResPkt);
 }
 
 int BodySizeManager::Get(unsigned short pktId)
