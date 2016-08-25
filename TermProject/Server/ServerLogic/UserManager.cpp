6b8545f0b5639de181d6f7cacb187d48f9c16c82
FeatherHub <woook95@naver.com>
2016-08-25 12:37:35 +0900

    Implement data get interface and packet process method

--- TermProject/Server/ServerLogic/UserManager.cpp
+++ TermProject/Server/ServerLogic/UserManager.cpp
@@ -5,13 +5,13 @@ void UserManager::Init(Network* network)
 {
 	m_pRefNetwork = network;
 
-	for (int i = 0; i < MAX_USER_NUM; i++)
+	for (int i = 0; i < MAX_LOBBY_USER_NUM; i++)
 	{
 		m_userIdxPool.push(i);
 	}
 }
 
-void UserManager::AddUser(char* id, int clientIdx)
+void UserManager::AddUser(const char* name, int clientIdx)
 {
 	int userIdx = GetUserPoolIdx();
 	
@@ -19,8 +19,9 @@ void UserManager::AddUser(char* id, int clientIdx)
 	
 	user.isConnected = true;
 	user.clientIdx = clientIdx;
-//	wcsncpy(user.id, id, wcslen(id));
-	strncpy(user.name, id, strlen(id));
+
+	CopyMemory(user.name, name, strlen(name));
+	user.name[strlen(name)] = '\0';
 
 	m_userNum++;
 }
@@ -31,6 +32,23 @@ int UserManager::GetUserPoolIdx()
 	m_userIdxPool.pop();
 
 	return idx;
+}
+
+const char* UserManager::FindUserName(int clientIdx)
+{
+	for (int i = 0; i < MAX_LOBBY_USER_NUM; i++)
+	{
+		const auto& user = m_userPool[i];
+		if (user.Connected() == true)
+		{
+			if (user.GetClientIdx() == clientIdx)
+			{
+				return user.GetName();
+			}
+		}
+	}
+
+	return nullptr;
 }
 
 void UserManager::RemoveUser(int clientIdx)
@@ -46,7 +64,7 @@ void UserManager::RemoveUser(int clientIdx)
 
 void UserManager::NotifyNewbieLogin(int newbieClientIdx, char* newbieName)
 {
-	for (int i = 0; i < MAX_USER_NUM; i++)
+	for (int i = 0; i < MAX_LOBBY_USER_NUM; i++)
 	{
 		if (m_userPool[i].Connected() == false)
 		{
@@ -60,6 +78,32 @@ void UserManager::NotifyNewbieLogin(int newbieClientIdx, char* newbieName)
 			continue;
 		}
 
-		m_pRefNetwork->SendPacket(otherClientIdx, PacketId::LoginNtf, newbieName);
+		LoginNtfPkt ntfPkt;
+		CopyMemory(ntfPkt.newbieName, newbieName, MAX_USER_NAME_LEN);
+
+		m_pRefNetwork->SendPacket(otherClientIdx, (unsigned short)PacketId::LoginNtf, (char*)&ntfPkt);
+	}
+}
+
+void UserManager::NotifyLobbyChatMsg(int senderClientIdx, const char* senderName, const char* msg)
+{
+	for (int i = 0; i < MAX_LOBBY_USER_NUM; i++)
+	{
+		const auto& user = m_userPool[i];
+		if (user.Connected() == false)
+		{
+			continue;
+		}
+
+		if (user.GetClientIdx() == senderClientIdx)
+		{
+			continue;
+		}
+
+		LobbyChatNtfPkt ntfPkt;
+		CopyMemory(ntfPkt.senderName, senderName, MAX_USER_NAME_LEN);
+		CopyMemory(ntfPkt.msg, msg, MAX_CHAT_MSG_LEN);
+
+		m_pRefNetwork->SendPacket(user.GetClientIdx(), PacketId::LobbyChatNtf, (char*)&ntfPkt);
 	}
 }
