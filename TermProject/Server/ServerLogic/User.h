0a458b93ec8aec48c9dfad305f93643ad4bc3a8b
FeatherHub <woook95@naver.com>
2016-08-25 12:38:02 +0900

    Add const getter interface

--- TermProject/Server/ServerLogic/User.h
+++ TermProject/Server/ServerLogic/User.h
@@ -6,9 +6,10 @@ struct User
 {
 	User() : isConnected(false) {}
 	
-	bool Connected() { return isConnected; }
-
-	char name[MAX_USER_NAME_LEN];
+	bool Connected() const { return isConnected; }
+	int GetClientIdx() const { return clientIdx; }
+	const char* GetName() const { return name; }
+	char name[MAX_USER_NAME_LEN+1];
 	int clientIdx;
 	bool isConnected = false;
 };
